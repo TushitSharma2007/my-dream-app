e0151f19df8806ad2c55ed8d0702b722f952c81d
Tushit Sharma <77721508+TushitSharma2007@users.noreply.github.com>
2021-11-12 06:01:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var background_img;
+var link;
+
+function preload(){
+    background_image = loadImage("mydp2.jpg");
+}
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    background_img = createSprite(windowWidth/2,windowHeight/2,windowWidth,windowHeight);
+    background_img.addImage("back_ground",background_image);
+    background_img.scale = 1.7;
+    link = createA('https://tushitsharma2007.github.io/my-dream-app/','Please tap here to begin')
+    link.position(windowWidth/2.20,windowHeight/1.8);
+    
+}
+
+function draw(){
+    fill("black");
+    
+    drawSprites();
+    textSize(70);
+    text("Helping hands", windowWidth/2.9, windowHeight/2.4);
+    textSize(40);
+    text("Tap the text below to begin",windowWidth/3,windowHeight/2);
+}
